9607de7a3ea6fee29059d86adda8fb210cd5dffe
Anna <annamorrison500@hotmail.com>
2020-04-24 16:02:45 +0100

    if/else argument on addition function
    - need to throw "error" if 2 values are entered which aren't of a number type.
    - example: two words/strings

--- scripts/calc.js
+++ scripts/calc.js
@@ -1,3 +1,9 @@
-function addition(numOne + numTwo) {
-    return numOne + numTwo;
+function addition(numOne, numTwo) {
+    //if a number type isn't returned then produce an error
+    if(typeof(numOne) == "number" && typeof(numTwo) == "number") {
+        return numOne + numTwo;
+    }
+    else {
+        return "Error!";
+    }
 }
